b52f795bb5b026d410b14a6fd80c933aad983360
Jonathan Dewandre <jonathandewandre@MacBook-Air-de-Jonathan-2.local>
2023-05-26 21:10:15 +0200

    début de la mise en place du header + menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,14 @@
-console.info('Hello world');
+"use strict"
+
+window.onscroll = function() {stickyMenu()};
+
+var menu = document.getElementById("sticky-menu");
+var sticky = menu.offsetTop;
+
+function stickyMenu() {
+    if (window.pageYOffset >= sticky) {
+        menu.classList.add("sticky");
+    }else{
+        menu.classList.remove("sticky");
+    }
+}
