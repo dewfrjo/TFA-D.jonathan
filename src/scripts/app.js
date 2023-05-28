f8686fe715dd18a30d1cebe191068fdb20a188d0
Jonathan Dewandre <jonathandewandre@MacBook-Air-de-Jonathan-2.local>
2023-05-28 17:36:46 +0200

    text et modif menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,18 @@
 "use strict"
 
-window.onscroll = function() {stickyMenu()};
-
-var menu = document.getElementById("sticky-menu");
-var sticky = menu.offsetTop;
-
-function stickyMenu() {
+window.onscroll = function() {
+    stickyMenu();
+  };
+  
+  var menu = document.getElementById("sticky-menu");
+  var sticky = menu.offsetTop;
+  
+  function stickyMenu() {
     if (window.pageYOffset >= sticky) {
-        menu.classList.add("sticky");
-    }else{
-        menu.classList.remove("sticky");
+      menu.classList.add("sticky");
+    } else {
+      menu.classList.remove("sticky");
     }
-}
+  }
+  
+
