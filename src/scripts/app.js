07b41d18437438287d2fd4f09d4a43f0641d353b
Jonathan Dewandre <jonathandewandre@MacBook-Air-de-Jonathan-2.local>
2023-06-01 14:40:03 +0200

    restructuration + changement de style

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,17 +8,17 @@ if(menuBurger){
     })
 }
 
-window.onscroll = function() {
-    stickyMenu();
-  };
+// window.onscroll = function() {
+//     stickyMenu();
+//   };
   
-  var menu = document.querySelector(".sticky-menu");
-  var sticky = menu.offsetTop;
+//   var menu = document.querySelector(".sticky-menu");
+//   var sticky = menu.offsetTop;
   
-  function stickyMenu() {
-    if (window.pageYOffset >= sticky) {
-      menu.classList.add("sticky");
-    } else {
-      menu.classList.remove("sticky");
-    }
-  }
+//   function stickyMenu() {
+//     if (window.pageYOffset >= sticky) {
+//       menu.classList.add("sticky");
+//     } else {
+//       menu.classList.remove("sticky");
+//     }
+//   }
