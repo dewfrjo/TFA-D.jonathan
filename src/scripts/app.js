ada515ded320c5f32b4fbb54eb951f9273c94737
Jonathan Dewandre <jonathandewandre@MacBook-Air-de-Jonathan-2.local>
2023-06-28 13:07:03 +0200

    modif pour aout - js corrigé

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -69,113 +69,14 @@ function updateMenuState() {
 // Ajouter un écouteur d'événement de défilement pour mettre à jour l'état du menu
 window.addEventListener('scroll', updateMenuState);
 
-
-
-// pour faire apparaitre ou disparaitre les el d de la partie avantage
-
-let togg1 = document.querySelector('.togg1');
-let togg2 = document.querySelector('.togg2');
-let togg3 = document.querySelector('.togg3');
-let togg4 = document.querySelector('.togg4');
-let togg5 = document.querySelector('.togg5');
-let togg6 = document.querySelector('.togg6');
-let d1 = document.querySelector('.d1');
-let d2 = document.querySelector('.d2');
-let d3 = document.querySelector('.d3');
-let d4 = document.querySelector('.d4');
-let d5 = document.querySelector('.d5');
-let d6 = document.querySelector('.d6');
-togg1.addEventListener("click", () => {
-    if(getComputedStyle(d1).display != "none"){
-        d1.style.display = "none";
-    }else{
-        d1.style.display = "block";
-    }
-})
-
-function togg(){
-    if(getComputedStyle(d2).display != "none"){
-        d2.style.display = "none";
-    }else{
-        d2.style.display = "block";
-    }
-};
-togg2.onclick = togg;
-
-togg3.addEventListener("click", () => {
-    if(getComputedStyle(d3).display != "none"){
-        d3.style.display = "none";
-    }else{
-        d3.style.display = "block";
-    }
-})
-togg4.addEventListener("click", () => {
-    if(getComputedStyle(d4).display != "none"){
-        d4.style.display = "none";
-    }else{
-        d4.style.display = "block";
-    }
-})
-togg5.addEventListener("click", () => {
-    if(getComputedStyle(d5).display != "none"){
-        d5.style.display = "none";
-    }else{
-        d5.style.display = "block";
-    }
-})
-togg6.addEventListener("click", () => {
-    if(getComputedStyle(d6).display != "none"){
-        d6.style.display = "none";
-    }else{
-        d6.style.display = "block";
-    }
-})
-
-// pour la partie lim
-
-let toggl1 = document.querySelector('.toggl1');
-let toggl2 = document.querySelector('.toggl2');
-let toggl3 = document.querySelector('.toggl3');
-let toggl4 = document.querySelector('.toggl4');
-let toggl5 = document.querySelector('.toggl5');
-let l1 = document.querySelector('.l1');
-let l2 = document.querySelector('.l2');
-let l3 = document.querySelector('.l3');
-let l4 = document.querySelector('.l4');
-let l5 = document.querySelector('.l5');
-toggl1.addEventListener("click", () => {
-    if(getComputedStyle(l1).display != "none"){
-        l1.style.display = "none";
-    }else{
-        l1.style.display = "block";
-    }
-})
-
-toggl2.addEventListener("click", () => {
-    if(getComputedStyle(l2).display != "none"){
-        l2.style.display = "none";
-    }else{
-        l2.style.display = "block";
-    }
-})
-toggl3.addEventListener("click", () => {
-    if(getComputedStyle(l3).display != "none"){
-        l3.style.display = "none";
-    }else{
-        l3.style.display = "block";
-    }
-})
-toggl4.addEventListener("click", () => {
-    if(getComputedStyle(l4).display != "none"){
-        l4.style.display = "none";
-    }else{
-        l4.style.display = "block";
-    }
-})
-toggl5.addEventListener("click", () => {
-    if(getComputedStyle(l5).display != "none"){
-        l5.style.display = "none";
-    }else{
-        l5.style.display = "block";
-    }
-})
+//pour faire pop ou non mon text dans la partie avantege et limite en version tel
+let togTriggers = document.querySelectorAll(".togg-trigger");
+for(let togTrigger of togTriggers){
+    togTrigger.addEventListener("click", (e) => {
+        let parentNode = e.currentTarget.parentNode;
+        let contentNode = parentNode.querySelector(".togg-content");
+        if(contentNode){
+            contentNode.classList.toggle("hidden");
+        } 
+    });
+}
